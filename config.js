29157705a0d7886ea8252fae4e4ea18a7e3303ac
getupandgo <getupandgo11@gmail.com>
2016-06-29 15:09:35 +0300

    removed twitter support

--- config.js
+++
@@ -1,8 +0,0 @@
-exports.twitterKey = "Consumer key";
-exports.twitterSecret = "Consumer secret";
-exports.accessToken = "Access token";
-exports.tokenSecret = "Access token secret";
-exports.brightness = "Indicator LED brightness file";
-exports.trigger = "Indicator LED trigger file";
-exports.message="New #BeagleBoneBlack @BeagleStache image captured!"
-
